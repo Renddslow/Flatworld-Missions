d5a8efbab680a26fc898d4f910639c78442c47f2
Matt McElwee <mubatt@wyopub.com>
2015-05-15 10:58:19 -0400

    Modified database.php to handle exceptions

--- database.php
+++ database.php
@@ -1,3 +1,11 @@
 <?php
 
-$db = new PDO("mysql:host=localhost;dbname=vitorioa_missionary","vitorioa_matt","Livis100%sexy")
+try {
+$db = new PDO("mysql:host=localhost;dbname=vitorioa_missionary","vitorioa_matt","Livis100%sexy");
+var_dump($db);
+} catch (Exception $e) {
+	echo "Could not connect to the database.";
+	exit;
+}
+
+echo "Woo-hoo!";
