29e78cfeffa1060a0474d456e303463f0c243ace
Matt McElwee <mubatt@wyopub.com>
2015-05-15 19:52:44 -0400

    Modified contact.php

--- inc/contact.php
+++ inc/contact.php
@@ -1,4 +1,32 @@
+<link rel="stylesheet" href="/style.css">
+
 <div class="contact">
 	<h1>Contact <?php echo $missionary ?></h1>
-	<input type="text">
+		<table>
+			<tr>
+				<th>
+					<label for="name">Name</label>
+				</th>
+				<td>
+					<input type="text" name="name" id="name">
+				</td>
+			</tr>
+			<tr>
+				<th>
+					<label for="email">Email</label>
+				</th>
+				<td>
+					<input type="text" name="email" id="email">
+				</td>
+			</tr>
+			<tr>
+				<th>
+					<label for="message">Message</label>
+				</th>
+				<td>
+					<textarea type="text" name="message" id="message"></textarea>
+				</td>
+			</tr>
+		</table>
+		<input type="submit" value="Send">
 </div>
