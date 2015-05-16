0effdf89f1eb10223b7506392bd23a7f167a6381
Matt McElwee <mubatt@wyopub.com>
2015-05-16 02:27:24 -0400

    Modified missionaries.php, hopefully to get it to display, got a little ahead of myself with functions and such

--- inc/missionaries.php
+++ inc/missionaries.php
@@ -1,6 +1,6 @@
 <?php
 
-function get_missionaries_all() {
+//function get_missionaries_all() {
 
 try {
 	$db = new PDO("mysql:host=localhost;dbname=vitorioa_missionary","vitorioa_matt","Livis100%sexy");
@@ -12,16 +12,19 @@ try {
 }
 
 try {
-	$results = $db->query("SELECT first_names, last_name, country, region, paragraph, img_path, alt, email FROM missionaries");
+	$results = $db->query("SELECT first_names, last_name, country, region, paragraph, img_path, alt, email FROM missionaries ORDER BY num ASC");
 	echo "Our query ran succesfully.";
 } catch (Exception $e) {
 	echo "Data could not be retrieved from the database.";
+	exit;
 }
 
 $missionaries = ($results->fetchAll(PDO::FETCH_ASSOC));
 
 return $missionaries;
 
-}
+//}
+//echo "<pre>";
+//var_dump($results->fetchAll(PDO::FETCH_ASSOC));
 
 ?>
